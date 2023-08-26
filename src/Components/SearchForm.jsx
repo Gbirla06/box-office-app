45d2fd9f141945b3622d5b437dcec4bcbb7208ac
Ganesh <gbirla06@gmail.com>
2023-08-26 15:21:12 +0530

    Fetch data inside show component

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState,useEffect} from "react";
 
 const SearchForm = ({onSearch}) =>{
     const [searchStr, setSearchStr] = useState("");
     const [searchOption, setSearchOption] = useState("shows");
     
+    /*
+    life Cycle of components
+      1) mounts
+      2) rerender
+      3) unmount
+    */
+   console.log("Component rerender");
+    useEffect(()=> {
+      console.log("Component mount")
+      return () =>{
+        console.log("Component unmount")
+      }
+    },[]);
+
     const onSearchInputChange = (ev) => {
         setSearchStr(ev.target.value);
       };
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,8 +1,37 @@
 import {useParams} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getShowById } from "../Api/tvmaz";
+
 const Show = () =>{
     const {showId} = useParams();
+
+    const [showData, setShowData] = useState(null);
+    const [showError,setShowError] = useState(null);
+
+    useEffect(() =>{
+        // getShowById();
+        async function fetchData(){
+            try{
+                const data = await getShowById(showId);
+                setShowData(data);
+            }
+            catch(err){
+                setShowError(err);
+            }
+        }
+
+        fetchData();
+    },[showId]);
+
+    if(showError){
+        return <div>We have an error : {showError.message}</div>
+    }
+    if(showData){
+        return <div>Got show data : {showData.name}</div>
+    }
+
     return <div>
-        Show Pages for {showId}
+        Data is loading
     </div>
 };
 
