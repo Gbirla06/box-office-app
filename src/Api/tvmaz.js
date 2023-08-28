01d57c6adb6486df305652b80a31a8705eaf018d
Ganesh <gbirla06@gmail.com>
2023-08-28 12:42:37 +0530

    stuck in error throw err

--- src/Api/tvmaz.js
+++ src/Api/tvmaz.js
@@ -10,3 +10,9 @@ const apiGet = async (queryString) => {
 export const searchForShows = (query) => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = (query) => apiGet(`/search/people?q=${query}`);
 export const getShowById = (showId) => apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowByIds = async (showIds) =>{
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`))
+  const result = await Promise.all(promises);
+  return result;
+}
--- src/Pages/Starred.jsx
+++ src/Pages/Starred.jsx
@@ -1,10 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import { useStarredShows } from "../Components/lib/useStarredShows";
-
+import { getShowByIds} from "../Api/tvmaz";
+import ShowGrid from "../Components/shows/ShowGrid";
+ 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
   
+  const {data:starredShows,error:starredshowsError} = useQuery({
+    queryKey:['starred',starredShowsIds],
+    queryFn:async () => getShowByIds(starredShowsIds)
+
+  })
+
+  if(starredShows){
+    return <ShowGrid />
+  }
+
   return <div>
-    Starred page {starredShows.length};
+    Starred page {starredShowsIds.length};
     </div>;
 };
 
